5589a9bb383200f74cb079e40d744d9c13666294
unknown <michaelangelo4489@gmail.com>
2025-03-10 10:48:22 +0700

    test reels

--- src/main.js
+++ src/main.js
@@ -5,7 +5,23 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div class="reels-container">
-    <div class="reel" id="skincare">Skincare</div>
+    <div class="reel" id="skincare">
+      <h1>Skincare<h1>
+      <div class="ig-ads-caption-container">
+        <div class="username-container">
+          <div class="logo-circle">L</div>
+          <div class="username-column-container">
+            <div class="username-verified">
+              <div class="username">Evoke</div>
+              <div class="verified">☑️</div>
+            </div>
+            <div class="sponsored">Sponsored</div>
+          </div>
+        </div>
+        <div class="description">Evoke is gud</div>
+        <div class="shop-now"><div>Shop Now</div></div>
+      </div>
+    </div>
     <div class="reel" id="haircare">Haircare</div>
     <div class="reel" id="bodycare">Bodycare</div>
     <div class="reel" id="perfume">Perfume</div>
