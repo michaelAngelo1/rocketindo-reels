a7c43f567625a9452320a3ed0553a8d6306a7009
unknown <michaelangelo4489@gmail.com>
2025-03-10 11:27:40 +0700

    back to story v1

--- src/main.js
+++ src/main.js
@@ -1,111 +1,24 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div class="reels-container">
     <div class="reel" id="skincare">
       <div>Skincare</div>
-      <div class="ig-ads-caption-container">
-        <div class="username-container">
-          <div class="logo-circle">L</div>
-          <div class="username-column-container">
-            <div class="username-verified">
-              <div class="username">Skincare</div>
-              <div class="verified">☑️</div>
-            </div>
-            <div class="sponsored">Sponsored</div>
-          </div>
-        </div>
-        <div class="description">Lorem ipsum dolor sit amet</div>
-        <div class="shop-now"><div>Shop Now</div></div>
-      </div>
     </div>
     <div class="reel" id="haircare">
       <div>Haircare</div>
-      <div class="ig-ads-caption-container">
-        <div class="username-container">
-          <div class="logo-circle">L</div>
-          <div class="username-column-container">
-            <div class="username-verified">
-              <div class="username">Haircare</div>
-              <div class="verified">☑️</div>
-            </div>
-            <div class="sponsored">Sponsored</div>
-          </div>
-        </div>
-        <div class="description">Lorem ipsum dolor sit amet</div>
-        <div class="shop-now"><div>Shop Now</div></div>
-      </div>
     </div>
     <div class="reel" id="bodycare">
       <div>Bodycare</div>
-      <div class="ig-ads-caption-container">
-        <div class="username-container">
-          <div class="logo-circle">L</div>
-          <div class="username-column-container">
-            <div class="username-verified">
-              <div class="username">Bodycare</div>
-              <div class="verified">☑️</div>
-            </div>
-            <div class="sponsored">Sponsored</div>
-          </div>
-        </div>
-        <div class="description">Lorem ipsum dolor sit amet</div>
-        <div class="shop-now"><div>Shop Now</div></div>
-      </div>
     </div>
     <div class="reel" id="perfume">
       <div>Perfume</div>
-      <div class="ig-ads-caption-container">
-        <div class="username-container">
-          <div class="logo-circle">L</div>
-          <div class="username-column-container">
-            <div class="username-verified">
-              <div class="username">Perfume</div>
-              <div class="verified">☑️</div>
-            </div>
-            <div class="sponsored">Sponsored</div>
-          </div>
-        </div>
-        <div class="description">Lorem ipsum dolor sit amet</div>
-        <div class="shop-now"><div>Shop Now</div></div>
-      </div>
     </div>
     <div class="reel" id="liptint">
       <div>Liptint</div>
-      <div class="ig-ads-caption-container">
-        <div class="username-container">
-          <div class="logo-circle">L</div>
-          <div class="username-column-container">
-            <div class="username-verified">
-              <div class="username">Liptint</div>
-              <div class="verified">☑️</div>
-            </div>
-            <div class="sponsored">Sponsored</div>
-          </div>
-        </div>
-        <div class="description">Lorem ipsum dolor sit amet</div>
-        <div class="shop-now"><div>Shop Now</div></div>
-      </div>
     </div>
     <div class="reel" id="maternity">
       <div>Maternity</div>
-      <div class="ig-ads-caption-container">
-        <div class="username-container">
-          <div class="logo-circle">L</div>
-          <div class="username-column-container">
-            <div class="username-verified">
-              <div class="username">Maternity</div>
-              <div class="verified">☑️</div>
-            </div>
-            <div class="sponsored">Sponsored</div>
-          </div>
-        </div>
-        <div class="description">Lorem ipsum dolor sit amet</div>
-        <div class="shop-now"><div>Shop Now</div></div>
-      </div>
     </div>
   </div>
 `
