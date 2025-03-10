47c716b0f12eb4aa8bb646d1c01283819f3d2cbd
unknown <michaelangelo4489@gmail.com>
2025-03-10 11:33:11 +0700

    video on skincare

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ document.querySelector('#app').innerHTML = `
   <div class="reels-container">
     <div class="reel" id="skincare">
       <div>Skincare</div>
+      <video autoplay muted loop>
+        <source src="https://cdn.shopify.com/videos/c/o/v/2662e3fa7a094e1badb6e4a0708e11e7.mp4" type="video/mp4">
+      </video>
     </div>
     <div class="reel" id="haircare">
       <div>Haircare</div>
