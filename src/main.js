a4a67ae8871a64648426033d6550c1594d5d2158
unknown <michaelangelo4489@gmail.com>
2025-03-10 10:28:02 +0700

    different color

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div class="reels-container">
-    <div class="reel">Skincare</div>
-    <div class="reel">Haircare</div>
-    <div class="reel">Bodycare</div>
-    <div class="reel">Perfume</div>
-    <div class="reel">Liptint</div>
-    <div class="reel">Maternity</div>
+    <div class="reel" id="skincare">Skincare</div>
+    <div class="reel" id="haircare">Haircare</div>
+    <div class="reel" id="bodycare">Bodycare</div>
+    <div class="reel" id="perfume">Perfume</div>
+    <div class="reel" id="liptint">Liptint</div>
+    <div class="reel" id="maternity">Maternity</div>
   </div>
 `
